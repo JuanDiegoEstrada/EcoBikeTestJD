0673a14d70c2ed2b41948cb4885421221ba2d4ad
Juan Diego Estrada Marcelino <jestrada@weecompany.net>
2024-10-21 13:21:40 -0600

    Initial Commit

---
+++ EcoBikeTestJD/EcoBikeTestJDApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoBikeTestJDApp.swift
+//  EcoBikeTestJD
+//
+//  Created by Juan Diego on 21/10/24.
+//
+
+import SwiftUI
+
+@main
+struct EcoBikeTestJDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
